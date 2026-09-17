58c45b36b4e5cff066a989006e7d053d6baac1c5
Paulo <paulofrattagrunho@gmail.com>
2026-09-17 08:17:23 -0300

    Adicionando estrutura inicial de pastas

---
+++ backend/src/test/java/com/juliuseconomyapi/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliuseconomyapi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
