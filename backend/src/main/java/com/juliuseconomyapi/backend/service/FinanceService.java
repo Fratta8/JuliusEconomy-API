9f0b60ce170dffc6b62edc90abb74a4a54d48cdb
Fratta8 <paulofrattagrunho@gmail.com>
2026-09-23 11:58:25 +0000

    service

--- backend/src/main/java/com/juliuseconomyapi/backend/service/FinanceService.java
+++ backend/src/main/java/com/juliuseconomyapi/backend/service/FinanceService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.juliuseconomyapi.backend.dto.FinanceDTO;
+import com.juliuseconomyapi.backend.mapper.FinanceMapper;
 import com.juliuseconomyapi.backend.model.Finance;
 import com.juliuseconomyapi.backend.repository.FinanceRepository;
 import com.juliuseconomyapi.backend.service.FinanceService;
@@ -18,6 +20,24 @@ public class FinanceService {
     //recebe o repository
     public FinanceService(FinanceRepository financeRepository) {
         this.financeRepository = financeRepository;
+    }
+
+    public List<FinanceDTO> getAllFinance(){
+        return financeRepository.findAll()
+        .stream()
+        .map(FinanceMapper::toDTO )
+        .toList();
+    }
+
+    public Optional<FinanceDTO> getFinanceById(Long id){
+        return financeRepository.findById(id)
+            .map(FinanceMapper::toDTO);
+    
+    }
+
+    public FinanceDTO createFinance(FinanceDTO financeDTO){
+        return FinanceMapper.toDTO(financeRepository.save(
+            FinaceMapper.toDTO()))
     }
     
 }
